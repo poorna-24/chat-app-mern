b748bbc0e05e8ab2a4e70df3c2883206d298abd7
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-06-21 02:05:06 +0530

    check email

---
+++ server/controller/checkEmail.js
@@ -0,0 +1,15 @@
+import UserModel from "./../models/userModel.js";
+import { errorHandler } from "./../utils/error.js";
+export const checkEmail = async (req, res, next) => {
+  const { email } = req.body;
+  const checkEmail = await UserModel.findOne({ email }).select("-password");
+
+  if (!checkEmail) {
+    next(errorHandler(500, "Email not found"));
+  }
+  res.status(200).json({
+    message: "Email verify",
+    success: true,
+    data: checkEmail,
+  });
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { registerUser } from "../controller/registerUserController.js";
+import { checkEmail } from "../controller/checkEmail.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/email", checkEmail);
 
 export default router;
